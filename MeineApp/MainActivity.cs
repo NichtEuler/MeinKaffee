135d286778d7875cc40934061516de864e5a3642
NichtEuler <horrerri67684@th-nuernberg.de>
2019-10-04 12:09:52 +0200

    Add project files.

---
+++ MeineApp/MainActivity.cs
@@ -0,0 +1,58 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System.Collections.Generic;
+using Android.Content;
+using Android.Views;
+using Java.Lang;
+
+namespace MeineApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            //InititalizeButton();
+            this.InitializeAlarm();
+        }
+
+        private void InititalizeButton()
+        {
+            Button button = FindViewById<Button>(Resource.Id.sendIntent);
+            button.Click += Button_Click;
+        }
+
+        private void Button_Click(object sender, System.EventArgs e)
+        {
+            Intent myIntent = new Intent(this, typeof(MyAlarmManager)); 
+            PendingIntent pendingIntent = PendingIntent.GetBroadcast(this, 0, myIntent,0);
+
+            AlarmManager alarmManager = (AlarmManager) GetSystemService(AlarmService);
+            alarmManager.Set(AlarmType.RtcWakeup, JavaSystem.CurrentTimeMillis()+(10*1000), pendingIntent);
+            Toast.MakeText(this, "Sending Intent", ToastLength.Short).Show();
+
+        }
+
+        private void InitializeAlarm()
+        {
+            MyAlarmManager manager = new MyAlarmManager();
+            this.RegisterReceiver(manager, new IntentFilter("com.urbandroid.sleep.alarmclock.ALARM_ALERT_START_AUTO"));
+            PendingIntent pendingIntent = PendingIntent.GetBroadcast(this, 0, new Intent("com.urbandroid.sleep.alarmclock.ALARM_ALERT_START_AUTO"), 0);
+            AlarmManager alarmManager = (AlarmManager)GetSystemService(AlarmService);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ MeineApp/MyAlarmManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Android.Content;
+using Android.Widget;
+using System.Net.Http;
+using Android.App;
+using Java.Lang;
+
+namespace MeineApp
+{
+    [BroadcastReceiver]
+    class MyAlarmManager : BroadcastReceiver
+    {
+        HttpClient client = new HttpClient();
+        Uri url = new Uri("http://192.168.178.55/toggleled");
+        Activity activity = new Activity();
+        public override void OnReceive(Context context, Intent intent)
+        {
+            Toast.MakeText(context, "http://192.168.178.55/toggleled", ToastLength.Short).Show();
+            PendingResult pendingResult = GoAsync();
+            new Thread(async () =>
+            {
+                try
+                {
+                    var reqResultAsync = await client.GetStringAsync(url);
+                    string checkResult = reqResultAsync.ToString();
+                    activity.RunOnUiThread(() =>
+                    {
+                        Toast.MakeText(context, checkResult, ToastLength.Long).Show();
+
+                    });
+
+                    client.Dispose();
+                    pendingResult.Finish();
+                }
+                catch (System.Exception ex)
+                {
+                    string checkResult = "Error " + ex.ToString();
+                    client.Dispose();
+                }
+            }).Start();
+        }
+    }
+}
