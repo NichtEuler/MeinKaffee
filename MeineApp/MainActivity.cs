3e7289c001bd05c88f2098b55213bfc9254a73c4
NichtEuler <horrerri67684@th-nuernberg.de>
2019-10-05 16:49:14 +0200

    enable/disable kaffe by switch

--- MeineApp/MainActivity.cs
+++ MeineApp/MainActivity.cs
@@ -7,31 +7,43 @@ using System.Collections.Generic;
 using Android.Content;
 using Android.Views;
 using Java.Lang;
+using Android.Support.V7.Widget;
 
 namespace MeineApp
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        MyAlarmManager kaffeeManager;
+        SwitchCompat actvKaffee;
+        Button button;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
-            //InititalizeButton();
-            this.InitializeAlarm();
+            kaffeeManager = new MyAlarmManager();
+            actvKaffee = FindViewById<SwitchCompat>(Resource.Id.activateKaffee);
+            button = FindViewById<Button>(Resource.Id.sendIntent);
+            InititalizeButtons();
+            InitializeAlarm();
         }
 
-        private void InititalizeButton()
+        private void InititalizeButtons()
         {
-            Button button = FindViewById<Button>(Resource.Id.sendIntent);
             button.Click += Button_Click;
+            actvKaffee.Click += ActvKaffee_Click;
+        }
+
+        private void ActvKaffee_Click(object sender, System.EventArgs e)
+        {
+            kaffeeManager.Activated = actvKaffee.Checked;
         }
 
         private void Button_Click(object sender, System.EventArgs e)
         {
-            Intent myIntent = new Intent(this, typeof(MyAlarmManager)); 
+            Intent myIntent = new Intent("com.urbandroid.sleep.alarmclock.ALARM_ALERT_START_AUTO"); 
             PendingIntent pendingIntent = PendingIntent.GetBroadcast(this, 0, myIntent,0);
 
             AlarmManager alarmManager = (AlarmManager) GetSystemService(AlarmService);
@@ -42,10 +54,9 @@ namespace MeineApp
 
         private void InitializeAlarm()
         {
-            MyAlarmManager manager = new MyAlarmManager();
-            this.RegisterReceiver(manager, new IntentFilter("com.urbandroid.sleep.alarmclock.ALARM_ALERT_START_AUTO"));
-            PendingIntent pendingIntent = PendingIntent.GetBroadcast(this, 0, new Intent("com.urbandroid.sleep.alarmclock.ALARM_ALERT_START_AUTO"), 0);
-            AlarmManager alarmManager = (AlarmManager)GetSystemService(AlarmService);
+            this.RegisterReceiver(kaffeeManager, new IntentFilter("com.urbandroid.sleep.alarmclock.ALARM_ALERT_START_AUTO"));
+            //PendingIntent pendingIntent = PendingIntent.GetBroadcast(this, 0, new Intent("com.urbandroid.sleep.alarmclock.ALARM_ALERT_START_AUTO"), 0);
+            //AlarmManager alarmManager = (AlarmManager)GetSystemService(AlarmService);
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
--- MeineApp/MyAlarmManager.cs
+++ MeineApp/MyAlarmManager.cs
@@ -11,25 +11,32 @@ namespace MeineApp
     class MyAlarmManager : BroadcastReceiver
     {
         HttpClient client = new HttpClient();
-        Uri url = new Uri("http://192.168.178.55/toggleled");
+        Uri url = new Uri("http://kaffeewecker/toggleled");
         Activity activity = new Activity();
+        bool activated = false;
+
+        public bool Activated { get => activated; set => activated = value; }
+
         public override void OnReceive(Context context, Intent intent)
         {
-            Toast.MakeText(context, "http://192.168.178.55/toggleled", ToastLength.Short).Show();
+            if (!activated)
+            {
+                return;
+            }
+            //Toast.MakeText(context, "http://192.168.178.55/toggleled", ToastLength.Short).Show();
             PendingResult pendingResult = GoAsync();
             new Thread(async () =>
             {
                 try
-                {
-                    var reqResultAsync = await client.GetStringAsync(url);
+                { var reqResultAsync = await client.GetStringAsync(url);
                     string checkResult = reqResultAsync.ToString();
                     activity.RunOnUiThread(() =>
                     {
-                        Toast.MakeText(context, checkResult, ToastLength.Long).Show();
+                        Toast.MakeText(context, "Kaffee" + checkResult, ToastLength.Long).Show();
 
                     });
 
-                    client.Dispose();
+                    //client.Dispose();
                     pendingResult.Finish();
                 }
                 catch (System.Exception ex)
